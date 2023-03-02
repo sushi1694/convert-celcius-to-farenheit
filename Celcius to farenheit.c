750a6d359935d49347218ea4189aae5c094e5011
sushi1694 <124113986+sushi1694@users.noreply.github.com>
2023-03-02 08:21:32 +0530

    Create Celcius to farenheit.c

---
+++ Celcius to farenheit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float celsius, fahrenheit;
+    printf("Enter Temperature Value (in Celsius): ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius*1.8)+32;
+    printf("\nEquivalent Temperature Value (in Fahrenheit) = %0.2f", fahrenheit);
+    getch();
+    return 0;
+}
